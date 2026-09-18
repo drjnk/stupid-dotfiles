48826e0c2927f4396b4844b2da9becaea4d12c0b
drjnk <ya.doverchiviy.loh@yandex.ru>
2026-09-18 23:05:54 +0300

    made simple installer

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install git sway dunst fuzzel neovim waybar wlogout swaylock bat xfce4-terminal blueman pavucontrol telegram-desktop obsidian playerctl flameshot kanshi nwg-look
+echo "Please, install manually:"
+echo "GTK Theme: Squared"
+echo "Icon theme: Palette Adwaita Yellow (Dark)"
+echo "Cursor theme: MacOS Pixel Dark"
